daa02d00994e933632b5899e4c7462a44bcc15fc
Aman Singh <singhaman7087@gmail.com>
2024-08-10 08:01:46 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./papes/Header";
+import Home from "./papes/Home";
+import Footer from "./papes/Footer";
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Home />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/papes/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+export default function Home() {
+  return (
+    <div>
+        <h1 className={styles.colorName}>Hi, this is Aman </h1>
+    </div>
+  )
+}
