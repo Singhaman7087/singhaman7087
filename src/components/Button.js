3ee0b1bf82ab3c533a4837ae07afc27828456b8b
Aman Singh <singhaman7087@gmail.com>
2024-08-10 08:06:19 +0530

    props btn

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Button({title}) {
+  return (
+    <div>
+        <button >
+            {title}
+        </button>
+    </div>
+  )
+}
--- src/papes/Home.jsx
+++ src/papes/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import styles from '../styles/Home.module.css'
+import Button from '../components/Button'
 export default function Home() {
   return (
     <div>
         <h1 className={styles.colorName}>Hi, this is Aman </h1>
+        <Button title={'Go to Link'}/>
+        <Button title={'Quick call'}/>
     </div>
   )
 }
